d1c374470ac4b28917c34b4c9f6a72604179c974
maikelrobier <maikelrobier@gmail.com>
2019-06-29 16:40:06 -0500

    Fix bugs in vim setup script

--- scripts/setup-vim.sh
+++ scripts/setup-vim.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Check to see if Homebrew is installed, and install it if it is not
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew ..."; \
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
 # install vim
 brew install vim
-brew install neovim
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -12,5 +15,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim +PlugInstall +qa
 
 # support NeoVim
+brew install neovim
+
+mkdir -p ~/.config/nvim
 cp -r ~/.vim/ ~/.config/nvim
 cp ~/.vimrc ~/.config/nvim/init.vim
