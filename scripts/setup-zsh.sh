04a90e2d7f53360929788f07266dd1844bcfa984
Maikel Ruiz <maikelrobier@gmail.com>
2021-09-20 19:53:28 -0500

    Install auto suggest plugin

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -1,2 +1,4 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
