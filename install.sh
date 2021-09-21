0cb4d4514321d36f8e7ce4f92a8803a4776fe6b7
Maikel Ruiz <maikelrobier@gmail.com>
2021-09-20 19:42:36 -0500

    Install zsh with configuration

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ cp -a ./home/. ~
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     bash ./scripts/setup-linux-vim.sh
+    bash ./scripts/setup-zsh.sh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     bash ./scripts/setup-macos-vim.sh
     bash ./scripts/setup-macos-vscode.sh
+    bash ./scripts/setup-zsh.sh
 # else
     # TODO
 fi
