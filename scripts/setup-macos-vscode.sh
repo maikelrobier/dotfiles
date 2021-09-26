c8cb62ce3570f9ec4125b14f2751d9117f450d06
maikelrobier <maikelrobier@gmail.com>
2021-09-26 17:45:49 -0500

    Fix VSCode Vim key repeat in macOS

--- scripts/setup-macos-vscode.sh
+++ scripts/setup-macos-vscode.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 cp vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+# https://stackoverflow.com/questions/39972335/how-do-i-press-and-hold-a-key-and-have-it-repeat-in-vscode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
